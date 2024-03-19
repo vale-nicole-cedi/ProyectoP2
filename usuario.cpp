9aa2d272c13e2d830e8373f6680b2a6774153e6b
Daniela Nuño <13044@portalcedi.edu.mx>
2024-03-19 13:27:23 -0600

    usuario.cpp

--- usuario.cpp
+++ usuario.cpp
@@ -4,3 +4,24 @@
 #include "usuario.h"
 
 using namespace std;
+
+int Usuario::getId(){
+return this->id;
+}
+
+void Usuario::mostrar(){
+    cout << "ID: " << this->id << endl;
+    cout << "Nombre: " << this->nombre << endl;
+    cout << "Edad: " << this->edad << endl;
+    cout << "Nacionalidad: " << this->nacionalidad << endl;
+}
+
+void Usuario::mostrarAmigos(){
+    for(int i=0; i<amigos.size(); i++){
+        cout << amigos[i]->nombre << endl;
+    }
+}
+
+void Usuario::mostrarPublicaciones(){
+    //mostrar todas las publicaciones del usuario
+}
